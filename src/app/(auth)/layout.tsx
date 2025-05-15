30d4f369f0a477daf8bf0ed0d9e97c06380530a8
Kayla Johnson <kaylaj8310@gmail.com>
2025-05-15 20:32:10 +0000

    Will you add the logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
 
 import type { ReactNode } from 'react';
+import AppLogo from '@/components/AppLogo';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-[calc(100vh-var(--header-height,4rem))] items-center justify-center bg-background p-4">
+    <div className="flex flex-col min-h-[calc(100vh-var(--header-height,4rem))] items-center justify-center bg-background p-4 space-y-8">
+      <AppLogo />
       {children}
     </div>
   );
