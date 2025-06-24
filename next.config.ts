b05a92cb16b8bff05f2a7a9add704a731ee4e99d
Kayla Johnson <kaylaj8310@gmail.com>
2025-06-24 11:08:19 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -17,6 +17,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  webpack: (config) => {
+    const originalIgnored = config.watchOptions.ignored || [];
+    config.watchOptions.ignored = [
+        ...(Array.isArray(originalIgnored) ? originalIgnored : [originalIgnored]),
+        /hosting\/.*/,
+    ];
+    return config;
   },
 };
 
